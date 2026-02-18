f8fd97026d5af1a052a5a2e024cadaf79adead19
MariamCHARRADA <charradamariam@yahoo.com>
2026-02-18 11:26:06 +0100

    add providers table using bootstrap and css styling

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Provider } from '../models';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class Home {
 
+  providers: Provider[] = [
+    { id: 1, name: 'Provider 1', email: 'provider1@gmail.com', address: 'Address 1' },
+    { id: 2, name: 'Provider 2', email: 'provider2@gmail.com', address: 'Address 2' },
+    { id: 3, name: 'Provider 3', email: 'procider2@gmail.com', address: 'Address 3' },
+  ];
 }
