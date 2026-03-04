7f37308f808fd2965f31db9aae11c4404430c1fe
MariamCHARRADA <charradamariam@yahoo.com>
2026-03-04 11:41:31 +0100

    deleteProvider

--- src/app/add-provider/add-provider.ts
+++ src/app/add-provider/add-provider.ts
@@ -30,4 +30,6 @@ export class AddProvider implements OnInit {
       }
     );
   }
+
+
 }
--- src/app/list-providers/list-providers.ts
+++ src/app/list-providers/list-providers.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Providers } from '../services/providers';
 import { Provider } from '../models';
 import { BehaviorSubject } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-providers',
@@ -11,13 +12,17 @@ import { BehaviorSubject } from 'rxjs';
 })
 export class ListOfProviders implements OnInit {
 
-  constructor(private providerService: Providers) { /* injecting dependencies */
+  constructor(private providerService: Providers, private router: Router) { /* injecting dependencies */
   }
 
   listProviders = new BehaviorSubject<Provider[]>([]); /* import type Provider from models */
 
   ngOnInit(): void {
-    this.providerService.getProviders().subscribe(
+    this.loadProviders();
+  }
+
+  loadProviders() {
+        this.providerService.getProviders().subscribe(
       {
         next: (data: any) => { /* next = si ok, error = si error */
           this.listProviders.next(data);
@@ -27,6 +32,20 @@ export class ListOfProviders implements OnInit {
           console.error('Error getting providers:', err);
         }
       });
+    }
+
+    deleteProvider(id: any) {
+    this.providerService.deleteProvider(id).subscribe(
+      {
+        next: (data) => {
+          this.loadProviders(); // Refresh the list after deletion
+          console.log(data);
+        },
+        error: (err) => {
+          console.error('Error deleting provider:', err); 
+        }
+      }
+    );
   }
 
 }
